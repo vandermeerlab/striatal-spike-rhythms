fa493f9e045b082a17311a348b4b405103b7ebbf
mvdm <mvdm@dartmouth.edu>
2018-09-03 16:36:56 -0400

    Converted Check functions for datatypes to logical
    Previously double, now logical. Also added basic tests for datatype check functions.

--- shared/datatypes/ts/CheckTS.m
+++ shared/datatypes/ts/CheckTS.m
@@ -8,7 +8,7 @@ function pass_flag = CheckTS(ts_in,varargin)
 %           callername: name of invoking function (for more informative warning messages)
 %
 %	OUTPUTS:
-%       pass_flag: 1 if all checks pass, 0 if otherwise
+%       pass_flag: True if all checks pass, False if otherwise
 %
 %	Checks performed:
 %       -  .t field must exist (FAIL)
@@ -23,7 +23,7 @@ function pass_flag = CheckTS(ts_in,varargin)
 % aacarey Nov 2015
 % youkitan edit Dec 2016, reformat help, add function name to output
 
-pass_flag = 1;
+pass_flag = true;
 
 in_mfun = '';
 if ~isempty(varargin) && ischar(varargin{1})
@@ -36,14 +36,14 @@ if ~isstruct(ts_in)
 else
     % check for .t field
     if ~isfield(ts_in,'t')
-        pass_flag = 0;
+        pass_flag = false;
         fprintf('FAIL%s by CheckTS: input ts must contain t field.\n',in_mfun);
         
     else
         % check that all t are column vectors
         for iT = 1:length(ts_in.t)
             if ~iscolumn(ts_in.t{iT})
-                pass_flag = 0;
+                pass_flag = False;
                 fprintf('FAIL%s by CheckTS: all contents of ts_in.t must be column vectors.\n',in_mfun);
             end
         end
@@ -51,22 +51,22 @@ else
     
     % check for label field
     if ~isfield(ts_in,'label')
-        %pass_flag = 0;
+        %pass_flag = false;
         fprintf('WARNING%s by CheckTS: ts_in lacks a .label field.\n',in_mfun);
     else
         % check that label is the same length as t
         if length(ts_in.label) ~= length(ts_in.t)
-            pass_flag = 0;
+            pass_flag = False;
             fprintf('FAIL%s by CheckTS: ts_in.t and ts_in.label must have the same length.\n',in_mfun);
         end
     end
 
     % check for type field
     if pass_flag && ~isfield(ts_in,'type')
-        %pass_flag = 0;
+        %pass_flag = false;
         fprintf('WARNING%s by CheckTS: input appears to be ts datatype but lacks the .type field.\n',in_mfun);
     elseif pass_flag && isfield(ts_in,'type') && ~strcmp(ts_in.type,'ts')
-        %pass_flag = 0;
+        %pass_flag = false;
         fprintf('WARNING%s by CheckTS: input appears to be ts datatype but is identified as %s in the .type field.\n',in_mfun,ts_in.type);
     end
     
--- shared/datatypes/tsd/CheckTSD.m
+++ shared/datatypes/tsd/CheckTSD.m
@@ -8,7 +8,7 @@ function pass_flag = CheckTSD(tsd_in,varargin)
 %           callername: name of invoking function (for more informative warning messages)
 %
 %	OUTPUTS:
-%       pass_flag: 1 if all checks pass, 0 if otherwise
+%       pass_flag: True if all checks pass, False if otherwise
 %
 %   Checks performed:
 %       -  .data field must be a cell (FAIL)
@@ -20,7 +20,7 @@ function pass_flag = CheckTSD(tsd_in,varargin)
 % MvdM 2014-06-24
 % youkitan edit Dec 2016, reformat help, add function name to output
 
-pass_flag = 1;
+pass_flag = true;
 
 in_mfun = '';
 if ~isempty(varargin) && ischar(varargin{1})
@@ -28,13 +28,13 @@ if ~isempty(varargin) && ischar(varargin{1})
 end
 
 if ~isstruct(tsd_in)
-   pass_flag = 0;
+   pass_flag = False;
    fprintf('FAIL%s by CheckTSD: data is not a struct.\n',in_mfun);
    return;
 end
 
 if iscell(tsd_in.data)
-   pass_flag = 0;
+   pass_flag = False;
    fprintf('FAIL%s by CheckTSD: data is a cell.\n',in_mfun);
    return;
 end
@@ -43,12 +43,12 @@ nSignals = size(tsd_in.data,1);
 nSamples_data = size(tsd_in.data,2);
 
 if nSignals > nSamples_data
-   %pass_flag = 0;
+   %pass_flag = False;
    fprintf('WARNING%s by CheckTSD: more signals (%d) than samples (%d) in data.\n',in_mfun,nSignals,nSamples_data);
 end
 
 nSamples_tvec = length(tsd_in.tvec);
 if nSamples_data ~= nSamples_tvec
-    pass_flag = 0;
+    pass_flag = false;
     fprintf('FAIL in %s by CheckTSD: samples in data (%d) does not match samples in tvec.\n',in_mfun,nSamples_data,nSamples_tvec);
 end
