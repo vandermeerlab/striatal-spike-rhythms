48236d8e16a73383ee2cfdeb971fd9632ebf5474
mvdm <mvdm@dartmouth.edu>
2019-02-20 12:38:00 -0500

    Updated CalcWVDistances() to return all measures

--- shared/util/CalcWVDistances.m
+++ shared/util/CalcWVDistances.m
@@ -3,16 +3,12 @@ function out = CalcWVDistances(cfg_in, out, out2)
 %
 % compute distances between all cell waveforms in out and all cell waveforms in out2
 % (out and out2 are the output of CategorizeStriatumWave.m)
-%
+
 cfg_def = [];
-cfg_def.method = 'SSE'; % {'SSE', 'SSEn', 'abs', 'absn', 'corr'}
-
 cfg = ProcessConfig(cfg_def, cfg_in);
-
 
 n1 = size(out.wv, 1);
 n2 = size(out2.wv, 1);
-
 
 for iX = 1:n1
     for iY = 1:n2
@@ -20,21 +16,17 @@ for iX = 1:n1
         wv1 = out.wv(iX, :);
         wv2 = out2.wv(iY, :);
         
-        switch cfg.method
-            case 'SSE'
-                out.wv_dist(iX, iY) = sum((wv1 - wv2).^2);
-            case 'SSEn' % normalize by energy of waveform 1
-                out.wv_dist(iX, iY) = sum((wv1 - wv2).^2);
-                out.wv_dist(iX, iY) = out.wv_dist(iX, iY) ./ sum((wv1 + wv2).^2);
-            case 'abs'
-                out.wv_dist(iX, iY) = sum(abs(wv1 - wv2));
-            case 'absn'
-                out.wv_dist(iX, iY) = sum(abs(wv1 - wv2));
-                out.wv_dist(iX, iY) = out.wv_dist(iX, iY) ./ sum((wv1 + wv2));
-            case 'corr'
-                temp = corrcoef(wv1, wv2);
-                out.wv_dist(iX, iY) = temp(1, 2);
-        end
+        out.SSE(iX, iY) = sum((wv1 - wv2).^2); % sum of squares
+        out.SSEn(iX, iY) = out.SSE(iX, iY) ./ sum((wv1 + wv2).^2); % normalized sum of squares
+        out.abs_diff(iX, iY) = sum(abs(wv1 - wv2)); % summed absolute difference
+        out.abs_diffn(iX, iY) = out.abs_diff(iX, iY) ./ sum((wv1 + wv2)); % normalized absolute difference
+                
+        temp = corrcoef(wv1, wv2);
+        out.corr(iX, iY) = temp(1, 2); % correlation
+        
+        [peak1_val, peak1_idx] = max(wv1);
+        out.peakdiff(iX, iY) = peak1_val - wv2(peak1_idx); % raw difference compared to peak of wv1
+        out.peakdiffn(iX, iY) = out.peakdiff(iX, iY) ./ peak1_val; % normalized difference (wv2 is what fraction of wv1 peak)
         
     end
 end
