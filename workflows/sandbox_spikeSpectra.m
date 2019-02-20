326900c9eccd4065ad51a2b5e8e28df6cd7aeea2
mvdm <mvdm@dartmouth.edu>
2019-02-20 12:38:12 -0500

    WIP on spike spectra main script

--- workflows/sandbox_spikeSpectra.m
+++ workflows/sandbox_spikeSpectra.m
@@ -1,27 +1,30 @@
 %% script exploring spike autocorrelations, spike spectra, and spike-triggered averages
 % TODO:
-% - collect cell type and mean firing rate
-% - find a way to skip duplicate cells
-% - skip cells if LFP is from same tt
+% - add SFC and/or PPC
+% - use alternate LFP if tt is same as LFP
 
 % remember to set path
 clear
-%addpath(genpath('D:\My_Documents\GitHub\striatal-spike-rhythms\chronux_2_12\spectral_analysis'));
 
-%fd = {'D:\data\adrlab\R117\R117-2007-06-20'};
-%fd = {'D:\data\adrlab\R132\R132-2007-10-20'};
-%fd = {'D:\data\R064\R064-2015-04-22'};
-%cd(fd{1});
+% set default interpreter to non-LaTeX
 
-please = [];
-please.rats = {'R117', 'R119', 'R131', 'R132'};
-[fd, fd_extra] = getDataPath(please);
+cfg_master = [];
+cfg_master.maxPrevCorr = 0.99; % if wv correlation with previous day is bigger than this, cell is possible duplicate
+cfg_master.maxPeakn = 0.1; % if peak wv difference (normalized) with previous day is smaller than this, cell is possible duplicate
+cfg_master.ccMethod = 'MvdM'; % method used for cell classification (see also 'Sirota')
+cfg_master.minSpikes = 100; % only keep cells with at least this many spikes
+cfg_master.plot = 0; % produce output figure for each cell?
+cfg_master.nShuf = 2; % number of spike shuffles
+cfg_master.trial_len = 50; % length (s) of trials to chop data up into
+cfg_master.rats = {'R117', 'R119', 'R131', 'R132'};
 
-please.plot = 0;
+[fd, fd_extra] = getDataPath(cfg_master);
 
 %%
 cc = 1; % cell count
-for iS = 1:length(fd)
+
+%%
+for iS = 2:length(fd)
 
     fprintf('Entering session %d/%d...\n',iS,length(fd));
     cd(fd{iS});
@@ -36,22 +39,25 @@ for iS = 1:length(fd)
         error('Couldn''t find LFP field name.');
     end
 
-    csc = LoadCSC(cfg); csc.data = csc.data-nanmean(csc.data); % may need to locdetrend
+    csc = LoadCSC(cfg); csc.data = csc.data-nanmean(csc.data); % could locdetrend to improve STA estimate
     
     lfp_tt = regexp(cfg.fc, 'CSC\d+', 'match');
-    lfp_tt = str2double(lfp_tt{1}{1}(4:end)); % need this to skip cells from same tt
+    lfp_tt = str2double(lfp_tt{1}{1}(4:end)); % need this to skip cells from same tt (could make into function)
     fprintf('LFP ttno is %d\n', lfp_tt);
 
     %% load data
-    cfg = [];
-    cfg.uint = '32';
+    cfg = []; cfg.uint = '32';
     S = LoadSpikes(cfg);
     S = restrict(S, ExpKeys.TimeOnTrack, ExpKeys.TimeOffTrack);
     
+    nSpikes = cellfun(@length, S.t);
+    keep = nSpikes >= cfg_master.minSpikes;
+    S = SelectTS([], S, keep);
+    
     %% Categorize cells and add tetrode depths
-    cfg_wv = [];
-    cfg_wv.cMethod = 'Sirota';
+    cfg_wv = [];cfg_wv.cMethod = cfg_master.ccMethod;
     s_out = CategorizeStriatumWave(cfg_wv, S);
+    
     s_out.unit = [s_out.other s_out.msn s_out.fsi];
     s_out.ident = [zeros(1, length(s_out.other)) ones(1, length(s_out.msn)) repmat(2, 1, length(s_out.fsi))];
     
@@ -69,69 +75,57 @@ for iS = 1:length(fd)
     end
     
     % correlate with previous session waveforms if available
-    if isempty(prev_fd)
-       S.usr.prevDist = Inf * ones(size(S.usr.tt_num)); 
+    if isempty(prev_fd) % no previous day available
+       S.usr.duplicate = zeros(size(S.usr.tt_num)); 
     else
        pushdir(prev_fd);
        S2 = LoadSpikes([]);
+       nSpikes = cellfun(@length, S2.t); keep = nSpikes >= cfg_master.minSpikes;
+       S2 = SelectTS([], S2, keep);
+     
        s_out2 = CategorizeStriatumWave(cfg_wv, S2);
-       
-       cfg_wv.method = 'corr'; % note, method chosen determines if min or max should be taken across cell pairs later!
-       s_out = CalcWVDistances(cfg_wv, s_out, s_out2);
+       s_out = CalcWVDistances([], s_out, s_out2); % add comparison with previous day's waveforms
        
        popdir;
-       
-       % for each cell, find max correlation with waveforms on same tetrode
+                     
+       % for each cell in current session, decide if duplicate
        for iC = 1:length(S.t)
           
            this_tt_no = S.usr.tt_num(iC);
            prev_day_cells = find(S2.usr.tt_num == this_tt_no);
            
-           temp_dist = s_out.wv_dist(iC, prev_day_cells);
-           if isempty(temp_dist) % no cells recorded fron this tt in previous session
-               S.usr.prevDist(iC) = Inf;
-           else
-               S.usr.prevDist(iC) = max(temp_dist); % note, should be appropriate for cfg_wv.method chosen above!
+           if isempty(prev_day_cells) % no cells recorded fron this tt in previous session
+               S.usr.duplicate(iC) = 0;
+           else % previous day cells found
+               temp_corr = s_out.corr(iC, prev_day_cells);
+               temp_peakn = s_out.peakdiffn(iC, prev_day_cells);
+               
+               if temp_corr > cfg_master.maxPrevCorr & abs(temp_peakn) < cfg_master.maxPeakn % wv correlation big, peak difference small
+                   S.usr.duplicate(iC) = 1;
+               else
+                   S.usr.duplicate(iC) = 0;
+               end
            end
-           
        end
     end
 
     %% Updated version with spike spectrum and STA
-    % TODO: SFC
-    cfg = [];
-    cfg.binsize = 0.001; cfg.Fs = 1./cfg.binsize;
-    cfg.nShuf = 20;
-    cfg.trial_len = 50;
+    % parameters for spike spectrum
+    cfg = []; cfg.binsize = 0.001; cfg.Fs = 1 ./ cfg.binsize;
+    cfg.nShuf = cfg_master.nShuf; cfg.trial_len = cfg_master.trial_len;
     
-    cfg.params = [];
-    cfg.params.Fs = 200;
-    cfg.params.tapers = [3 5];
-    
-    %clear bigP* STA*
+    cfg.params = []; cfg.params.Fs = 200; cfg.params.tapers = [3 5];
     
     tbin_edges = firstSpike(S):cfg.binsize:lastSpike(S);
     trial_starts = tbin_edges(1):cfg.trial_len:tbin_edges(end);
     
     for iC = length(S.t):-1:1
         
-        fprintf('Cell %d/%d...\n',iC,length(S.t));
-        
-        spk_t = S.t{iC};
-        if length(spk_t) < 100
-            fprintf('Cell skipped - insufficient spikes.\n');
-            continue;
-        end
+        fprintf('Cell %d/%d...\n', iC, length(S.t));
         
         % skip if not turned & correlated with prev session
-        if S.usr.distanceTurned(iC) < 80 & S.usr.prevDist(iC) > 0.99
-            fprintf('Cell skipped - WV too similar.\n');
-            continue;
-        end
-        
-        % skip if not msn or fsi
-        if S.usr.cell_type(iC) == 0
-            fprintf('Cell skipped - unidentified type (not MSN or FSI).\n');
+        if S.usr.duplicate(iC) & S.usr.distanceTurned(iC) < 80
+            fprintf('Cell skipped - likely duplicate.\n');
             continue;
         end
         
@@ -141,17 +135,16 @@ for iS = 1:length(fd)
             continue;
         end
         
-        % track some things about this cell here (cell type etc.)
+        spk_t = S.t{iC};
         
         % binarize spike train for acorr (display only)
-        spk_binned = histc(spk_t,tbin_edges); spk_binned = spk_binned(1:end-1);
-        [acf,tvec] = ComputeACF(cfg,spk_binned);
+        spk_binned = histc(spk_t, tbin_edges); spk_binned = spk_binned(1:end-1);
+        [acf, tvec] = ComputeACF(cfg, spk_binned);
         
-        if please.plot
-            figure;
-            subplot(221);
+        if cfg_master.plot
+            figure; subplot(221);
             plot(tvec,acf); xlim([-0.5 0.5]);
-            title(sprintf('Cell %d',iC));
+            title(sprintf('Cell %s', ALL_cellLabel{cc}));
         end
         
         % spike spectrum
@@ -167,7 +160,7 @@ for iS = 1:length(fd)
         keep = F > 1;
         P = 10*log10(P(keep)); F = F(keep);
         
-        if please.plot
+        if cfg_master.plot
             subplot(223);
             plot(F, medfilt1(P, 201), 'k', 'LineWidth', 2);
             drawnow;
@@ -178,18 +171,20 @@ for iS = 1:length(fd)
             continue;
         end
         
-        bigP(cc,:) = P; F_acf = F;
-        ALL_sessno(cc) = iS;
+        % track some things about this cell
+        ALL.cellType(cc) = S.usr.cell_type(iC);
+        ALL.cellLabel{cc} = S.label(iC);
+        ALL.cellDepth(cc) = S.usr.tetrodeDepths(iC);
+         
+        ALL.bigP(cc,:) = P; F_acf = F; % spike spectrum
+        ALL.sessno(cc) = iS;
+        ALL.ratID(cc) = fd_extra.ratID_num(iS);
         
         % shuffles for spike spectrum
         clear this_shufP;
-        for iShuf = cfg.nShuf:-1:1
+        for iShuf = cfg_master.nShuf:-1:1
             
             % shuffle spike train
-            %spk_shuf = cat(1, tbin_edges(1), spk_t); % isi shuffle -- too conservative for spike spectrum
-            %isi = diff(spk_shuf); isi = isi(randperm(length(isi)));
-            %spk_shuf = cat(1, tbin_edges(1), isi);
-            %spk_shuf = cumsum(spk_shuf); spk_shuf = spk_shuf(2:end);
             spk_shuf = rand(length(spk_t), 1);
             spk_shuf = tbin_edges(1) + spk_shuf .* (tbin_edges(end) - tbin_edges(1));
             
@@ -207,70 +202,66 @@ for iS = 1:length(fd)
             P = 10*log10(P(keep)); F = F(keep);
             
             this_shufP(iShuf,:) = P;
-        end
+        end % of shuffles
         
-        bigP_shufmean(cc,:) = nanmean(this_shufP);
-        bigP_shufSD(cc,:) = nanstd(this_shufP);
+        ALL.bigP_shufmean(cc,:) = nanmean(this_shufP);
+        ALL.bigP_shufSD(cc,:) = nanstd(this_shufP);
         
-        if please.plot
+        if cfg_master.plot
             hold on;
-            %plot(F,10*log10(bigP_shufmean(iC,:)),'r');
-            %plot(F,10*log10(bigP_shufmean(iC,:)+2*bigP_shufSD(iC,:)),'r:');
-            %plot(F,10*log10(bigP_shufmean(iC,:)-2*bigP_shufSD(iC,:)),'r:');
-            plot(F, bigP_shufmean(cc,:),'r', 'LineWidth', 2);
-            plot(F, bigP_shufmean(cc,:)+bigP_shufSD(cc,:),'r');
-            plot(F, bigP_shufmean(cc,:)-bigP_shufSD(cc,:),'r');
+            plot(F, ALL.bigP_shufmean(cc,:), 'r', 'LineWidth', 2);
+            plot(F, ALL.bigP_shufmean(cc,:) + ALL.bigP_shufSD(cc,:), 'r');
+            plot(F, ALL.bigP_shufmean(cc,:) - ALL.bigP_shufSD(cc,:), 'r');
             drawnow;
         end
         
         % get STA by binning on CSC timebase
         this_spk_binned = zeros(size(csc.tvec));
-        idx = nearest_idx3(spk_t,csc.tvec);
+        idx = nearest_idx3(spk_t, csc.tvec);
         this_spk_binned(idx) = 1;
-        [xc,tvec] = xcorr(csc.data,this_spk_binned,1000);
-        tvec = tvec.*median(diff(csc.tvec));
+        [xc, tvec] = xcorr(csc.data, this_spk_binned, 1000);
+        tvec = tvec .* median(diff(csc.tvec));
         
-        STA_all(cc,:) = xc;
+        ALL.STA(cc,:) = xc;
         
-        if please.plot
+        if cfg_master.plot
             subplot(222);
-            plot(tvec,xc);
+            plot(tvec, xc);
             title('raw STA');
         end
         
-        % transform
-        %[xc,tvec] = xcorr(csc.data,this_spk_binned);
-        this_Fs = 1./median(diff(csc.tvec));
-        [P,F] = pwelch(xc,length(xc),[],[],this_Fs);
+        % STA spectrum
+        this_Fs = 1 ./ median(diff(csc.tvec));
+        [P,F] = pwelch(xc,length(xc), [], [], this_Fs);
         
-        if please.plot
+        if cfg_master.plot
             subplot(224);
-            plot(F,10*log10(P)); xlim([0 100]);
+            plot(F, 10*log10(P)); xlim([0 100]);
             title('STA spectrum');
         end
         
-        STAp_all(cc,:) = P;
+        ALL.STAp(cc,:) = P;
         
         % do STA shuffles
         clear this_shufSTA this_shufSTAp;
         for iShuf = cfg.nShuf:-1:1
             spk_binned_shuf = this_spk_binned(randperm(length(this_spk_binned)));
-            this_shufSTA(iShuf,:) = xcorr(csc.data,spk_binned_shuf,1000);
-            this_shufSTAp(iShuf,:) = pwelch(this_shufSTA(iShuf,:),length(this_shufSTA(iShuf,:)),[],[],this_Fs);
+            this_shufSTA(iShuf,:) = xcorr(csc.data, spk_binned_shuf,1000);
+            this_shufSTAp(iShuf,:) = pwelch(this_shufSTA(iShuf,:), length(this_shufSTA(iShuf,:)), [], [], this_Fs);
         end
-        STA_shufmean(cc,:) = nanmean(this_shufSTA); STAp_shufmean(cc,:) = nanmean(this_shufSTAp);
-        STA_shufSD(cc,:) = nanstd(this_shufSTA); STAp_shufSD(cc,:) = nanstd(this_shufSTAp);
+        ALL.STA_shufmean(cc,:) = nanmean(this_shufSTA); ALL.STAp_shufmean(cc,:) = nanmean(this_shufSTAp);
+        ALL.STA_shufSD(cc,:) = nanstd(this_shufSTA); ALL.STAp_shufSD(cc,:) = nanstd(this_shufSTAp);
         
-        if please.plot
+        if cfg_master.plot
             subplot(222); hold on;
-            plot(tvec,STA_shufmean(cc,:),'r');
-            plot(tvec,STA_shufmean(cc,:)+2*STA_shufSD(cc,:),'r:');
-            plot(tvec,STA_shufmean(cc,:)-2*STA_shufSD(cc,:),'r:');
+            plot(tvec,ALL.STA_shufmean(cc,:),'r');
+            plot(tvec,ALL.STA_shufmean(cc,:) + 2*ALL.STA_shufSD(cc,:), 'r:');
+            plot(tvec,ALL.STA_shufmean(cc,:) - 2*ALL.STA_shufSD(cc,:), 'r:');
             
             subplot(224); hold on;
-            plot(F,10*log10(STAp_shufmean(cc,:)),'r');
-            plot(F,10*log10(STAp_shufmean(cc,:)+2*STAp_shufSD(cc,:)),'r:');
-            plot(F,10*log10(STAp_shufmean(cc,:)-2*STAp_shufSD(cc,:)),'r:');
+            plot(F,10*log10(ALL.STAp_shufmean(cc,:)), 'r');
+            plot(F,10*log10(ALL.STAp_shufmean(cc,:) + 2*ALL.STAp_shufSD(cc,:)), 'r:');
+            plot(F,10*log10(ALL.STAp_shufmean(cc,:) - 2*ALL.STAp_shufSD(cc,:)), 'r:');
             
             drawnow;
         end
@@ -279,15 +270,16 @@ for iS = 1:length(fd)
     end % of cell loop
     
 end % of session loop
+
 %% compute some z-scores and plot
 f_int = 1:0.5:100;
 
-bigPi = interp1(F_acf, bigP', f_int);
-bigPz = (bigP - bigP_shufmean) ./ bigP_shufSD;
+bigPi = interp1(F_acf, ALL.bigP', f_int);
+bigPz = (ALL.bigP - ALL.bigP_shufmean) ./ ALL.bigP_shufSD;
 bigPzi = interp1(F_acf, bigPz', f_int);
 
-STApi = interp1(F, STAp_all', f_int);
-STApz = (STAp_all - STAp_shufmean) ./ STAp_shufSD;
+STApi = interp1(F, ALL.STAp', f_int);
+STApz = (ALL.STAp - ALL.STAp_shufmean) ./ ALL.STAp_shufSD;
 STApzi = interp1(F, STApz', f_int);
 
 keep = ~isnan(sum(bigPz, 2));
@@ -382,6 +374,7 @@ xlabel('frequency (Hz)'); ylabel('correlation'); title('SS x STA (raw)');
 xval = f_int;
 keep = pval_raw < 0.05;
 hold on; plot(xval(keep),cc_raw(keep),'.b','MarkerSize',20);
+
 %%
 function [acf,tvec] = ComputeACF(cfg,spk_binned)
 if isfield(cfg,'maxlag')
